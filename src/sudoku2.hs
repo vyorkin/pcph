17906a7d5c5e75e46be761836e01735ce8e4419f
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2019-06-13 17:07:31 +0300

    Add an example of dynamic partitioning

--- src/sudoku2.hs
+++ src/sudoku2.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import GHC.Conc (numCapabilities)
 import System.Environment (getArgs)
 import Sudoku (solve)
 import Data.Maybe (isJust)
@@ -7,14 +8,34 @@ import Control.Monad (void)
 import Control.DeepSeq (force)
 import Control.Parallel.Strategies (runEval, rpar, rseq)
 
--- Total   time    0.054s  (  1.260s elapsed)
--- ------------------ VS --------------------
--- Total   time    0.041s  (  0.840s elapsed)
+-- Static partitioning
 
--- ~ x 1.5
+-- Compiling:
+-- cabal new-build sudoku2 --ghc-options="-eventlog"
+
+-- Running:
+-- cabal new-exec sudoku2 sudoku17.1000.txt -- +RTS -s -l -RTS
+
+-- The argument +RTS -s instructs the GHC runtime system to emit the statistics
+-- shown.
+
+-- As of GHC v6.12, you can leave off the number of cores and all available
+-- cores will be used (you still need to pass -N however, like so: +RTS -N).
+
+-- Total   time    0.054s  (1.260s elapsed)
+-- ------------------ VS ------------------
+-- Total   time    0.041s  (0.840s elapsed)
+
+-- wall-clock time difference:
+-- ---------------------------
+-- 1.260 / 0.041 ~= x 1.5
+
+-- Try to avoid partitioning the work into a small,
+-- fixed number of chunks
 
 main :: IO ()
 main = do
+  putStrLn $ "N (cores) = " ++ show numCapabilities
   [f]  <- getArgs
   file <- readFile f
   let
@@ -28,3 +49,7 @@ main = do
       return (as' ++ bs')
 
   print (length (filter isJust solutions))
+
+-- What we did here is called static partitioning
+-- And this is considered an anti-pattern
+-- Next time we're going to try dynamic partitioning
---
+++ src/sudoku3.hs
@@ -0,0 +1,50 @@
+module Main (main) where
+
+import System.Environment (getArgs)
+import Sudoku (solve)
+import Data.Maybe (isJust)
+import Control.Parallel.Strategies (Eval, runEval, rpar)
+
+-- Dynamic partitioning
+
+-- Compiling:
+-- cabal new-build sudoku3 --ghc-options="-eventlog"
+
+-- Running:
+-- cabal new-exec sudoku3 sudoku17.1000.txt -- +RTS -s -l -RTS
+
+-- sudoku1:  Total   time    0.030s  (  1.331s elapsed)
+-- ----------------------- VS -------------------------
+-- sudoku3:  Total   time    0.028s  (  0.311s elapsed)
+
+-- wall-clock time difference:
+-- ---------------------------
+-- 1.331 / 0.311 ~= x 4.27
+
+-- SPARKS: 1000(1000 converted, 0 overflowed, 0 dud, 0 GC'd, 0 fizzled)
+
+-- We created exactly 1,000 sparks, and they were all converted
+-- (that is, turned into real parallelism at runtime).
+
+main :: IO ()
+main = do
+  [f]  <- getArgs
+  file <- readFile f
+  let
+    puzzles = lines file
+    solutions = runEval (parMap solve puzzles)
+  print (length (filter isJust solutions))
+
+-- | Applies a function to a list in parallel.
+--
+-- Runs down the whole list, eagerly creating sparks for the application of the
+-- given function to each element, and finally returns the new list.
+--
+-- When parMap returns, it will have created
+-- one spark for each element of the list
+parMap :: (a -> b) -> [a] -> Eval [b]
+parMap _ [] = return []
+parMap f (a:as) = do
+  b  <- rpar (f a)
+  bs <- parMap f as
+  return $ b:bs
